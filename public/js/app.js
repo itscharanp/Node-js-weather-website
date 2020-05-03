b3143cbcfacf1cf22f7abfda1822b405ce02c7d0
Your Name <you@example.com>
2020-05-03 15:06:49 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit',(e)=>{
 
     mssgone.textContent = 'Loading..'
     mssgtwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error)
         {
